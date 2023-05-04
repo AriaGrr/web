d8ba0d7e5dc977a83a6a6b24d7f6156348103aeb
Wanderson Feitosa <wanderson.feitoso@mtn.com.br>
2023-05-04 18:06:09 -0300

    atualizações de qualidade

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var http = require('http');
-
-var express = require('express');
-
+var http = require("http");
+var express = require("express");
 var app = express();
 
-app.use(express.static('./public/projects.html'));
+app.use(express.static("./public"));
 
 var server = http.createServer(app);
 
