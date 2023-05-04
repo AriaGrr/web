405762d564a6a7e8a9f8b081d1252e779b25c234
AriaGrr <marjorieluize@outlook.com>
2023-05-04 19:02:42 -0300

    adiciona o sistema de login e cadastro

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var http = require("http");
-var express = require("express");
-var app = express();
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
-var server = http.createServer(app);
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
-server.listen(80);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-console.log("Running at http://localhost:80");
+const server = http.createServer(app);
+
+app.post('/cadastrar', (req, res) => {
+  const nome = req.body.nome;
+  const senha = req.body.senha;
+  res.render('cadastro', { nome, senha });
+});
+
+app.get('/login', (req, res) => {
+  const nome = req.query.nome;
+  const senha = req.query.senha;
+  res.render('login', { nome, senha });
+});
+
+server.listen(80, () => {
+  console.log('Running at http://localhost:80');
+});
