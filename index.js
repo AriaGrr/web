bf3539a543f5526342769484f9d43956b5bdfc20
AriaGrr <marjorieluize@outlook.com>
2023-05-16 06:37:03 -0300

    Corrigir erro

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/web');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/web', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexão com o MongoDB estabelecida com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  });
 
 app.use(express.static("./public"));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
 
 logins = [];
 products = [];
@@ -28,7 +30,7 @@ app.get("/", (req, res) => {
 app.post("/cadastrar", (req, res) => {
   const nome = req.body.nome;
   const senha = req.body.senha;
-  let mensagem = "Cadastro relizado com sucesso";
+  let mensagem = "Cadastro realizado com sucesso";
 
   // Verifica se o limite máximo de usuários foi atingido
   if (logins.length >= 10) {
@@ -93,13 +95,13 @@ app.post("/login", (req, res) => {
   }
 });
 
-// Produtos
+// Rota POST para lidar com solicitações de produtos
 app.post("/products", (req, res) => {
   const produto = req.body.produto;
   const valor = req.body.valor;
   let mensagem = "Produto cadastrado com sucesso";
 
-  //   // Verifica se o limite máximo de usuários foi atingido
+  // Verifica se o limite máximo de produtos foi atingido
   if (products.length >= 10) {
     mensagem = "Limite máximo de produtos atingido";
     res.render("products", {
@@ -109,23 +111,12 @@ app.post("/products", (req, res) => {
     });
     return;
   }
-
-  // Verifica se o login já existe no array
-  const existingProduct = products.find((l) => l.produto === produto);
-  if (existingProduct) {
-    mensagem = "Produto já cadastrado";
-    res.render("products", {
-      produto: "",
-      valor: "",
-      mensagem,
-    });
-    return;
-  }
-  // Adiciona o novo login ao array
+  // Adiciona o novo produto ao array
   products.push({
     produto,
     valor
   });
+
   res.render("products", {
     produto: "Produto: " + produto,
     valor: "Preço: " + valor,
@@ -137,15 +128,63 @@ app.get("/listUsers", (req, res) => {
   res.render("listUsers", {
     logins
   });
-})
+});
+
 app.get("/listProducts", (req, res) => {
   res.render("listProducts", {
     products
   });
-})
+});
 
+// Definindo o esquema para os posts
+const postSchema = new mongoose.Schema({
+  titulo: String,
+  resumo: String,
+  conteudo: String
+});
 
-server.listen(80, () => {
+// Criando um modelo para os posts com base no esquema
+const Post = mongoose.model('Post', postSchema);
+
+// Rota POST para lidar com solicitações de blog
+app.post("/blog", (req, res) => {
+  const titulo = req.body.titulo;
+  const resumo = req.body.resumo;
+  const conteudo = req.body.conteudo;
+
+  // Criando uma nova instância do modelo Post
+  const novoPost = new Post({
+    titulo,
+    resumo,
+    conteudo
+  });
+
+  // Salvando o novo post no banco de dados
+  novoPost.save((err) => {
+    if (err) {
+      console.error(err);
+      // Tratar o erro de salvamento, se necessário
+      res.render("error", { mensagem: "Erro ao salvar o post" });
+    } else {
+      res.render("success", { mensagem: "Post salvo com sucesso" });
+    }
+  });
+});
+
+// Rota GET para a página do blog
+app.get("/blog", (req, res) => {
+  // Recupere os posts do banco de dados usando o modelo Post
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.error(err);
+      // Tratar o erro de recuperação dos posts, se necessário
+      res.render("error", { mensagem: "Erro ao recuperar os posts do blog" });
+    } else {
+      res.render("blog.ejs", { posts });
+    }
+  });
+});
+
+app.listen(80, () => {
   console.log("Running at http://localhost:80");
-
 });
